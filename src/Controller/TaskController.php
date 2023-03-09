a14c6cd894c8832e7a633c488c570fd45d05fc37
Sofia Romera <sofia-romera@outlook.com>
2023-03-09 12:00:30 +0100

    Change main route, add style to form

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -2,14 +2,28 @@
 
 namespace App\Controller;
 
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
 class TaskController extends AbstractController
 {
 
-    public function index(): Response
+    // public function __construct(private ManagerRegistry $doctrine)
+    // {
+    // }
+
+    public function index(ManagerRegistry $doctrine): Response
     {
+        //Replace
+        $em = $this->$doctrine->getManager();
+        $task_repo = $this->$doctrine->getRepository();
+        $tasks = $task_repo->findAll();
+
+        foreach ($tasks as $task) {
+            echo $task->getTitle() . '<br/>';
+        }
+
         return $this->render('task/index.html.twig', [
             'controller_name' => 'TaskController',
         ]);
